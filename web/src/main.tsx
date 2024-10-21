87d0d831f5b7b96049f6d281086048f3bc02bc31
devhindo <lildevhind@gmail.com>
2024-10-21 16:29:59 +0300

    fixed some issues

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,24 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import ErrorPage from './errors/error-page.tsx'
-import Chat from './routes/chat.tsx'
 import './index.css'
 import {
-  createBrowserRouter,
   BrowserRouter,
-  RouterProvider,
   Routes,
   Route,
 } from "react-router-dom";
-import * as reactRouterDom from "react-router-dom";
 
 
-import Navbar from './components/navbar.tsx'
-import Root from './routes/root.tsx';
 import Signup from './components/signup.tsx'
+import App from './App.tsx';
 
-
+/*
 const router = createBrowserRouter([
   
   {
@@ -31,20 +24,23 @@ const router = createBrowserRouter([
     element: < Signup />,
   }
 ]);
+*/
 
-
+/*
 const isLoggedIn = (): boolean => {
   return !!localStorage.getItem('token');
 }
+*/
 
 createRoot(document.getElementById('root')!).render(
-
+  <StrictMode>
       <BrowserRouter>
         <Routes>
         
-        <Route path="/" element={<Root />} />  {/*or other routes*/}
+        <Route path="/" element={<App />} />  {/*or other routes*/}
         <Route path="/signup" element={<Signup />} />
         </Routes>
       </BrowserRouter>
+  </StrictMode>
 ,
 )

42e4d24d620a820537c3f12755159233c65180db
devhindo <lildevhind@gmail.com>
2024-11-22 08:12:03 +0200

    Add country field to users table and create posts table with relationships

--- scripts/db/schema.sql
+++ scripts/db/schema.sql
@@ -10,6 +10,7 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(255) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
+    country VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -17,3 +18,14 @@ CREATE TABLE IF NOT EXISTS users (
 INSERT INTO users (username, email, password) VALUES
 ('test', 'test@mail.com', 'test'),
 ('test2', 'test2@mail.com', 'test2');
+
+--name: posts
+CREATE TABLE IF NOT EXISTS posts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    author_id INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    content TEXT NOT NULL,
+    likes INT DEFAULT 0,
+    comments_number INT DEFAULT 0,
+    FOREIGN KEY (author_id) REFERENCES users(id)
+);
